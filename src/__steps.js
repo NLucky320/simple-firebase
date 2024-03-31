e8cac5947a28cfad45f2ce3a33616cd72fa5ab38
Nlucky320 <luckyes144@gmail.com>
2024-03-31 13:11:04 +0600

    first commit

---
+++ src/__steps.js
@@ -0,0 +1,12 @@
+/* 
+1. visit: console.firebase.google.com
+2. create project(skip google analytics)
+3. register app (create config)
+4. install firebase: npm install firebase
+5. add config file to your project
+6. DANGER: do not publish or make firebase to public by pushing to github
+7. visit: go to docs > build > authentication > web > get started
+8. export app from the firebase.config.js file : export default app;
+9. Login.jsx: import getAuth from firebase/auth
+10. create const auth = getAuth(app)
+*/
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLALdzZIFdg30Q8dbykYD2VjOkb26dIfg",
+  authDomain: "simple-firebase-77dae.firebaseapp.com",
+  projectId: "simple-firebase-77dae",
+  storageBucket: "simple-firebase-77dae.appspot.com",
+  messagingSenderId: "528641498265",
+  appId: "1:528641498265:web:18784b04693c26e3792c08",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
